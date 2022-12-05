afcafe7b71ce2b0b64aebebf3434137cb87efdf4
Nikhil Mhatre <nikhil.mhatre93@gmail.com>
2022-12-05 19:47:16 +0530

    Webserver Added

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+)
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("Hello..")
+	io.WriteString(w, "Hello..")
+	return
+}
+
+func Form(w http.ResponseWriter, r *http.Request) {
+	name := r.FormValue("name")
+	fmt.Println(name)
+	io.WriteString(w, "form data is "+name)
+	return
+}
+
+func FlowChartDiagram(w http.ResponseWriter, r *http.Request) {
+	buf, _ := ioutil.ReadFile("flow-chart.png")
+	w.Header().Add("Content-Type", "image/png")
+	w.Write(buf)
+	return
+}
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/hello", Hello)
+	http.HandleFunc("/form", Form)
+	http.HandleFunc("/flow-chart-diagram", FlowChartDiagram)
+
+	http.ListenAndServe(":8080", nil)
+}
